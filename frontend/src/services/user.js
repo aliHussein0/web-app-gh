e7ba5d41128438c2cc64dbe395c7f81b5c28ce69
Ali Hussein <72148411+aliHussein0@users.noreply.github.com>
2021-11-24 20:57:32 +0200

    update user.js v1

--- frontend/src/services/user.js
+++ frontend/src/services/user.js
@@ -22,30 +22,6 @@ class UserDataService {
     return http.delete(`/user-delete?user_id=${user_id}`)
   }
 
-  /*get(id) {
-    return http.get(`?id=${id}`);
-  }
-
-  find(query, by = "name", page = 0) {
-    return http.get(`?${by}=${query}&page=${page}`);
-  } 
-
-  createReview(data) {
-    return http.post("/review-new", data);
-  }
-
-  updateReview(data) {
-    return http.put("/review-edit", data);
-  }
-
-  deleteReview(id, userId) {
-    return http.delete(`/review-delete?id=${id}`, {data:{user_id: userId}});
-  }
-
-  getCuisines() {
-    return http.get(`/cuisines`);
-  }
-  */
 }
 
 export default new UserDataService();
